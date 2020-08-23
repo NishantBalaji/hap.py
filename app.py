715dd950b2142a87cc231216272f3fdda800a9e2
aidenszeto <aszeto35@gmail.com>
2020-08-22 21:17:22 -0700

    c

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import json
+import requests
 
 app = Flask(__name__)
 
@@ -8,5 +10,22 @@ def index():
 
 @app.route('/results')
 def results():
-    
-    return render_template()
+    subscription_key = 'b2cf0902bf004b89b2a5096c2df5e0e3'
+    assert subscription_key
+
+    face_api_url = 'https://spotifai.cognitiveservices.azure.com/face/v1.0/detect'
+
+    image_url = str(request.args.get('url'))
+
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+
+    params = {
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+    }
+
+    response = requests.post(face_api_url, params=params,
+                             headers=headers, json={"url": image_url})
+    data = json.dumps(response.json())
+    return render_template('results.html', data=data)
