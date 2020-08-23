f30bbfbed0550b100d8dedd608780271b98d7640
aidenszeto <aszeto35@gmail.com>
2020-08-23 00:21:31 -0700

    i

--- app.py
+++ app.py
@@ -35,30 +35,10 @@ def results():
 
     response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
     age = response.json()[0]['faceAttributes']['age']
-    emotion = response.json()[0]['faceAttributes']['emotion']
+    emotion = response.json()[0]['faceAttributes']['emotion']['happiness']
 
+    if
 
-    # Yelp API
-
-    headers = {
-        'authorization': f'Bearer {YELP_API}'
-    }
-
-    params = {
-        'term': 'happy'
-    }
-
-    param_string = urllib.parse.urlencode(params)
-    conn = http.client.HTTPSConnection("api.yelp.com")
-    conn.request("GET", "/v3/businesses/search?"+param_string, headers=headers)
-
-    res = conn.getresponse()
-    data = res.read()
-    data = json.loads(data.decode("utf-8"))
-
-    burl = data['businesses'][0]['url']
-
-    print(burl)
 
 
     return render_template('results.html', age=age, emotion=emotion)
